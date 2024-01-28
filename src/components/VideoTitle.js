18aea6340e1c646214bdb6cd8b6473619f50a094
akhr1997 <ak.hr1997@gmail.com>
2024-01-28 23:27:17 +0530

    Implement home page background video playback

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
-      <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="py-6 text-lg w-1/4">{overview}</p>
+    <div className="w-screen aspect-video pt-[30%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-6xl font-bold text-white">{title}</h1>
+      <p className="py-6 text-lg w-1/4 text-white">{overview}</p>
       <div className="">
-        <button className="bg-gray-500 text-white p-4 px-10 text-xl rounded-lg bg-opacity-50">
+        <button className="bg-white text-black p-4 px-10 text-xl rounded-lg hover:bg-opacity-65">
           ▶️ Play Now
         </button>
-        <button className="bg-gray-500 mx-2 text-white p-4 px-12 text-xl rounded-lg bg-opacity-90">
-          More Info ℹ
+        <button className="bg-gray-500 mx-2 text-white p-4 px-12 text-xl rounded-lg bg-opacity-90 hover:bg-opacity-65">
+          More Info
         </button>
       </div>
     </div>
